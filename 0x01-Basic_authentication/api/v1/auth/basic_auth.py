81397d687fab327dd862294559107cc4a43e39ce
Dayc <kongranalanyuy@gmail.com>
2024-11-12 21:58:51 +0000

    Allow passwords with ':'

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -45,7 +45,8 @@ class BasicAuth(Auth):
         if ':' not in decoded_base64_authorization_header:
             return (None, None)
         else:
-            credentials = decoded_base64_authorization_header.split(':')
+            # slipt the decoded credentials once with a ':' delimeter
+            credentials = decoded_base64_authorization_header.split(':', 1)
             return (credentials[0], credentials[1])
 
     def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
