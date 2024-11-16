2e4341f52848bce8d49b62e0a496cafef9f02f8b
Dayc <kongranalanyuy@gmail.com>
2024-11-16 16:06:01 +0000

    checked

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -5,6 +5,7 @@ from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models.user import User
 
+
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
 def view_all_users() -> str:
     """ GET /api/v1/users
@@ -126,6 +127,7 @@ def update_user(user_id: str = None) -> str:
         user.last_name = rj.get('last_name')
     user.save()
     return jsonify(user.to_json()), 200
+
 
 @app_views.route('/users/me', methods=['GET'], strict_slashes=False)
 def show_me():
